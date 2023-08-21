e14b66009c8e5339d5ab8ee7b3bbb5b72468ed28
simao234430 <simao234430@gmail.com>
2023-08-21 11:32:48 +0800

    rm dist

--- dist/index.d.ts
+++
@@ -1,4 +0,0 @@
-export type { ButtonProps } from './Button/interface';
-export { default as Button } from './Button';
-export { default as ConfigProvider } from './ConfigProvider';
-export type { ConfigProviderProps } from './ConfigProvider/interface';
